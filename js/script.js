a9829dc01ed4e6386df283f8c5a6182fb9c4de5e
marc0sniet0 <marcos.nieto.iglesias@outlook.es>
2025-12-08 13:32:44 +0100

    cabecera en index

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// Selección de elementos del HTML
+let burger = document.querySelector(".burger > i");
+let menu_opt = document.querySelector(".menu");
+
+// Definimos evento click sobre el boton del menú
+burger.addEventListener("click", function() {
+
+    burger.classList.toggle("fa-bars");
+    burger.classList.toggle("fa-times");
+    menu_opt.classList.toggle("menu-show");
+
+});
+
+
+
 
 (() => {
   const texts = document.querySelectorAll(".text-block");
