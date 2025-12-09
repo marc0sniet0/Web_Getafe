0c259fbf460863ff1b804e0e4b6c4e85302c33a3
marc0sniet0 <marcos.nieto.iglesias@outlook.es>
2025-12-09 17:33:58 +0100

    mejoras del modal y cabecera en visit

--- js/script.js
+++ js/script.js
@@ -64,28 +64,3 @@ window.addEventListener('scroll', () => {
 
 
 
-/* /////////VISIT IMPRES///////// */
-
-(() => {
-  const texts = document.querySelectorAll(".text-block");
-  const images = document.querySelectorAll(".background img");
-  const total = texts.length;
-
-  window.addEventListener("scroll", () => {
-    const scrollY = window.scrollY;
-    const vh = window.innerHeight;
-
-    // índice según la sección de scroll
-  
-    const index = Math.floor(scrollY / (vh * 0.8));
-
-
-    texts.forEach((el, i) => {
-      if (i === index) {
-        el.classList.add("active");
-      } else {
-        el.classList.remove("active");
-      }
-    });
-  });
-})();
--- js/visit_impres.js
+++ js/visit_impres.js
@@ -1,3 +1,16 @@
+// Selección de elementos del HTML
+let burger = document.querySelector(".burger > i");
+let menu_opt = document.querySelector(".menu");
+
+// Definimos evento click sobre el boton del menú
+burger.addEventListener("click", function() {
+
+    burger.classList.toggle("fa-bars");
+    burger.classList.toggle("fa-times");
+    menu_opt.classList.toggle("menu-show");
+
+});
+
 // ------ Scroll + crossfade + draggables persistentes ------
 (() => {
   const imgs = Array.from(document.querySelectorAll(".stage img"));
